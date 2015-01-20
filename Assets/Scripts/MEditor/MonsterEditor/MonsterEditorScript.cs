bd2d2a14a811373b28371cb529242dc00a2b0fea
Benny Peake <rayantares13@gmail.com>
2015-01-20 00:14:52 -0500

    Added monster editor

---
+++ Assets/Scripts/MEditor/MonsterEditor/MonsterEditorScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterEditorScript : MonoBehaviour
+{
+    void Start()
+    {
+        Song song = SongLoader.Instance.GetSong();
+
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void AddMonster()
+    {
+        string[] monsterTypes = new string[0];
+
+        //selectorDialogueCreator.CreateDialogue(monsterTypes, OnMonsterCreated, "Select a monster type!", "There are no monster types.");
+        OnMonsterCreated(null);
+    }
+
+    private void OnMonsterCreated(string result)
+    {
+        //if (string.IsNullOrEmpty(result))
+           // return;
+
+        RectTransform monsterElement = Instantiate(monsterElementTemplate) as RectTransform;
+
+        monsterElement.SetParent(monsterElementTemplate.parent, false);
+        monsterElement.SetSiblingIndex(monsterElement.GetSiblingIndex() - 1);
+        monsterElement.gameObject.SetActive(true);
+    }
+
+    [SerializeField]
+    private SelectorDialogueCreator selectorDialogueCreator;
+
+    [SerializeField]
+    private RectTransform monsterElementTemplate;
+}
