a0a636bfbb37b9be2811ff42c06388b11086af41
benkorza <bnkorza@wpi.edu>
2015-01-21 17:30:10 -0500

    Revises main menu screen
    Main menu now links to editor

--- Assets/Scripts/Core/Menu UI/Selection_UI.cs
+++ Assets/Scripts/Core/Menu UI/Selection_UI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Selection_UI : MonoBehaviour {
 
 	public GUIStyle bigFont;
-	
+
 	// Use this for initialization
 	void Start () {
 
@@ -12,26 +12,32 @@ public class Selection_UI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnGUI() {
 		GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+		GUI.Box(new Rect(Screen.width/(3.5f), Screen.height/6 - 10, (Screen.width - Screen.width/(3.5f)*2), Screen.height*(2/3f) + 10), "");
+		GUI.Label(new Rect(Screen.width/2, Screen.height/6 + 45, 0, 0), "Main Menu", bigFont);
 		if (GUI.Button (new Rect (Screen.width * (9f/10f), Screen.height * (9f/10f), 100, 40), "Quit")) {
 			// Quit the application
 			Application.Quit();
 		}
-		if (GUI.Button (new Rect (Screen.width/8 - 10, Screen.height/4, Screen.width*(2f/8f), Screen.height/2), "Play Game")) {
+		if (GUI.Button (new Rect (Screen.width/2 - 90, Screen.height*(1.5f/6f) + Screen.height/24 + 30, 180, Screen.height/16), "Play Game")) {
 			// Change scenes to gameplay
 			Application.LoadLevel ("Game");
 		}
-		if (GUI.Button (new Rect (Screen.width*3/8, Screen.height/4, Screen.width*(2f/8f), Screen.height/2), "View Stats")) {
+		if (GUI.Button (new Rect (Screen.width/2 - 90, Screen.height*(2.16f/6f) + Screen.height/24 + 30, 180, Screen.height/16), "View Stats")) {
 			// Change to stats screen
 			Application.LoadLevel ("Stats");
 		}
-		if (GUI.Button (new Rect (Screen.width*5/8 + 10, Screen.height/4, Screen.width*(2f/8f), Screen.height/2), "Unlock Things!")) {
+		if (GUI.Button (new Rect (Screen.width/2 - 90, Screen.height*(2.83f/6f) + Screen.height/24 + 30, 180, Screen.height/16), "Unlock Things!")) {
 			// Change to stats screen
 			Application.LoadLevel ("unlocks");
+		}
+		if (GUI.Button (new Rect (Screen.width/2 - 90, Screen.height*(3.5f/6f) + Screen.height/24 + 30, 180, Screen.height/16), "Go to Editor Menu")) {
+			// Change to stats screen
+			Application.LoadLevel ("EditorMainMenu");
 		}
 	}
 }
