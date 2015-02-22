8eeccf505ee6b5c65bf1bb7a22d050b8a2114edd
bpeake13 <rayantares13@gmail.com>
2015-02-21 20:36:11 -0500

    Issue #21 Add in player healthBar
    Its been added
    
    Tested By: Killing the palyer
    
    Reviewer #21

---
+++ Assets/Scripts/Core/Gameplay/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Counter))]
+public class Player : MonoBehaviour 
+{
+    public static Player Instance
+    {
+        get { return instance; }
+    }
+
+    void Start()
+    {
+        instance = this;
+        healthCounter = GetComponent<Counter>();
+        healthCounter.OnCounterChanged.AddListener(OnHealthChanged);
+    }
+
+    public void Hurt(int damage)
+    {
+        healthCounter.subtract(damage);
+    }
+
+    void OnHealthChanged(int value)
+    {
+        if(value <= 0)
+        {
+            OnKilled();
+        }
+    }
+
+    protected virtual void OnKilled()
+    {
+        Application.LoadLevel("GameOver");
+    }
+
+    private Counter healthCounter;
+
+    private static Player instance;
+}
---
+++ Assets/Scripts/Core/Gameplay/PlayerDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerDisplay : MonoBehaviour
+{
+    public void UpdateHealthBar(int health)
+    {
+        healthBar.value = (int)health;
+    }
+
+    [SerializeField]
+    private Slider healthBar;
+}
